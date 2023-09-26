1f4296e05a17cfaa634c38388ebc2c7e2dda85f2
Sergio Augusto Moreira Bastos Junior <sergiobastosfisio@yahoo.com.br>
2023-09-26 12:20:08 -0300

    upload e swagger

---
+++ src/controllers/upload.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+
+const aws = require("aws-sdk");
+
+const endpoint = new aws.Endpoint(process.env.BACKBLAZE_ENDPOINT_S3);
+const s3 = new aws.S3({
+    endpoint,
+    credentials: {
+        accessKeyId: process.env.BACKBLAZE_KEYID,
+        secretAccessKey: process.env.BACKBLAZE_APPLICATIONKEY,
+    },
+});
+
+const uploadImg = async (req: Request, res: Response) => {
+    const { id, table } = req.params
+
+    const imagem: any = req.file;
+
+    try {
+        if (!imagem) throw new Error("Nenhum arquivo enviado")
+        const fileUpload = await s3
+            .upload({
+                Bucket: process.env.BACKBLAZE_BUCKET,
+                Key: `${imagem.originalname}_${id}_${table}`,
+                Body: imagem.buffer,
+                ContentType: imagem.mimetype,
+            })
+            .promise();
+        res.json({ fileUpload });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Erro ao fazer o upload do arquivo" });
+    }
+};
+
+module.exports = { uploadImg };
