5916e5dbc5ba5d8f2cbe5735eb92a48898f7bfd1
Sergio Augusto Moreira Bastos Junior <sergiobastosfisio@yahoo.com.br>
2023-09-26 21:25:38 -0300

    tasks e pequenas alterações

---
+++ src/controllers/task/task.create.ts
@@ -0,0 +1,25 @@
+import { createOrUpdate, deleteOne, findFirst, findMany, findUnique } from "../../prismaFunctions/prisma";
+import { Request, Response } from "express";
+
+async function createTask(req: Request, res: Response): Promise<any> {
+    const data = req.body;
+
+    try {
+        const findTask = await findFirst("Task", { title: { contains: data.title, } });
+        console.log(findTask);
+
+        if (findTask) throw new Error("Tarefa ja foi criada");
+
+        const task = await createOrUpdate("Task", { ...data });
+
+        res.status(201).json({ mensagem: "Tarefa criada com sucesso" });
+
+    } catch (error: any) {
+        console.log(error);
+
+        res.status(400).json({ mensagem: error.message });
+
+    }
+}
+
+module.exports = { createTask }
