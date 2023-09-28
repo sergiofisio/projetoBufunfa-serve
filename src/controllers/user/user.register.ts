04b5eeba54fd0875b80db5bba7b5b483f694253a
Sergio Augusto Moreira Bastos Junior <sergiobastosfisio@yahoo.com.br>
2023-09-28 12:07:55 -0300

    logica para email unico

--- src/controllers/user/user.register.ts
+++ src/controllers/user/user.register.ts
@@ -9,12 +9,13 @@ async function register(req: Request, res: Response) {
     const { table } = req.params;
     const { name, email, password, cpf } = ceoSchema.parse(req.body);
 
-    const findEmail = await findUnique(table, { email });
+    const findEmailinCeo = await findUnique('ceo', { email });
+    const findEmailinEmployee = await findUnique('employee', { email });
 
-    if (findEmail) {
+    if (findEmailinCeo || findEmailinEmployee) {
       return res
         .status(400)
-        .json({ message: "Já existe um usuário com o email informado" });
+        .json({ message: "Já existe um usuário com o email cadastrado no nosso sistema" });
     }
 
     const findCpf = await prisma[table].findUnique({ where: { cpf } });
