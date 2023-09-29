2de4b1cb02ad067fbb1dd367a3f8a6ae85a9ba26
Sergio Augusto Moreira Bastos Junior <sergiobastosfisio@yahoo.com.br>
2023-09-29 15:49:03 -0300

    config depoy

--- prisma/seeds/statusTask.seed.js
+++ prisma/seeds/statusTask.seed.js
@@ -3,14 +3,21 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function seedTask() {
-  await prisma.statusTask.createMany({
-    data: [
-      { status: "a fazer" },
-      { status: "enviado" },
-      { status: "validar" },
-      { status: "feita" },
-    ],
-  });
+  const existingStatuses = await prisma.statusTask.findMany();
+  const statusesToCreate = [
+    { status: "a fazer" },
+    { status: "enviado" },
+    { status: "validar" },
+    { status: "feita" },
+  ].filter(
+    (status) => !existingStatuses.some((s) => s.status === status.status)
+  );
+
+  if (statusesToCreate.length > 0) {
+    await prisma.statusTask.createMany({
+      data: statusesToCreate,
+    });
+  }
 }
 
 module.exports = seedTask;
