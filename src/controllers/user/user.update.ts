fd440b98603bcdea6f62f9c3bbb7369b277d33a7
Sergio Augusto Moreira Bastos Junior <sergiobastosfisio@yahoo.com.br>
2023-10-31 16:58:58 -0300

    alterações rota editar usuario

--- src/controllers/user/user.update.ts
+++ src/controllers/user/user.update.ts
@@ -11,7 +11,26 @@ const update = async (req: Request, res: Response): Promise<any> => {
     try {
         const user = await findUnique(table, { id: Number(id) });
 
+        const cpfFormated = data.cpf.replace(/\D/g, "");
+
+        data.cpf = cpfFormated
+
         if (!user) throw new CustomError("Funcionário não encontrado", 400);
+
+        const findUserCeo = await findUnique("ceo", { email: data.email });
+
+        const findUserEmployee = await findUnique("employee", { email: data.email });
+
+        const findCpfCeo = await findUnique("ceo", { cpf: cpfFormated });
+
+        console.log(findCpfCeo);
+
+
+        const findCpfEmployee = await findUnique("employee", { cpf: cpfFormated });
+
+        if ((findUserCeo && findUserCeo.id !== Number(id)) || (findUserEmployee && findUserEmployee.id === Number(id))) throw new CustomError("Email ja cadastrado no sistema", 400)
+
+        if ((findCpfCeo && findCpfCeo.id !== Number(id)) || (findCpfEmployee && findCpfEmployee.id === Number(id))) throw new CustomError("Cpf ja cadastrado no sistema", 400)
 
         if (Number(user.id) !== Number(id) && user.type !== table) throw new Error("Você não tem acesso a este recurso")
 
